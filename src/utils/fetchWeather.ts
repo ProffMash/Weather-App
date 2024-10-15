789c8e378670be8a0bbc97e9ad1e615ecaafc8a6
david <kdavidmacharia8485@gmail.com>
2024-10-15 20:03:10 +0300

    fetchWeather added

---
+++ src/utils/fetchWeather.ts
@@ -0,0 +1,12 @@
+// utils/fetchWeather.ts
+export const fetchWeather = async (location: string) => {
+    const apiKey = 'a68df739a20d4390a5904009242603';
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3&aqi=no&alerts=no`
+    );
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data');
+    }
+    return response.json();
+  };
+  
